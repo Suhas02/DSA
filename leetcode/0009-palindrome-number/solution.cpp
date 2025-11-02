73bf97a04039758e84d034589814cb6ee44a316b
Suhas <72826050+Suhas02@users.noreply.github.com>
2025-11-02 16:32:43 +0000

    [LeetCode Sync] Runtime - 5 ms (19.61%), Memory - 8.5 MB (64.91%)

---
+++ leetcode/0009-palindrome-number/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        long long rev = 0, temp = x;
+        while(temp!=0){
+            int dig = temp % 10;
+            rev = (rev*10) + dig;
+            temp /= 10;
+        }
+        return rev==x;
+    }
+};
