b72107ccf61276bc29ac02c05e321680fb204d73
Suhas <72826050+Suhas02@users.noreply.github.com>
2026-03-03 11:48:10 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.2 MB (80.85%)

---
+++ leetcode/0054-spiral-matrix/solution.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> result;
+
+        int top = 0;
+        int bottom = matrix.size()-1;
+        int left = 0;
+        int right = matrix[0].size()-1;
+
+        //Traverse the matrix in Spiral Order
+        while(top<=bottom && left<=right){
+
+            for(int i=left;i<=right;i++){
+                result.push_back(matrix[top][i]);
+            }
+            top++;
+
+            for(int i=top;i<=bottom;i++){
+                result.push_back(matrix[i][right]);
+            }
+            right--;
+
+            //If Rows are remaining
+            if(top<=bottom){
+                for(int i = right;i>=left;i--){
+                    result.push_back(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+
+            //If Columns are Remaining 
+            if(left<=right){
+                for(int i=bottom;i>=top;i--){
+                    result.push_back(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        return result;
+    }
+};
