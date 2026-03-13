758423791b9ff2c610b5a2395f73eaf756ecf14f
Suhas <72826050+Suhas02@users.noreply.github.com>
2026-03-13 08:19:53 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 31.4 MB (44.93%)

---
+++ leetcode/0792-binary-search/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+     int binarySearch(vector<int>& nums, int low, int high, int target){
+        //Base Case
+        if(low > high) return -1;
+        int mid = (low + high)/2;
+        if(nums[mid] == target) return mid;
+        else if (nums[mid]<target) 
+             return binarySearch(nums, mid+1,high,target);
+        else  return binarySearch(nums,low,mid-1,target);
+     }
+    int search(vector<int>& nums, int target) {
+        //Recursive Function
+     return binarySearch(nums,0,nums.size()-1, target);
+    }
+};
